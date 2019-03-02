34c8336da396399740f659a1f28c18ac8b9d94a9
imlufeng <48149924+imlufeng@users.noreply.github.com>
2019-03-02 17:17:37 +0800

    Add files via upload

---
+++ weather-widget.js
@@ -0,0 +1,13 @@
+﻿tpwidget("init", {
+    "flavor": "bubble",
+    "location": "WSKKEVYQ9QHK",
+    "geolocation": "disabled",
+    "position": "bottom-left",
+    "margin": "10px 10px",
+    "language": "zh-chs",
+    "unit": "c",
+    "theme": "chameleon",
+    "uid": "UBB95E7970",
+    "hash": "66dabf1665e764812eeba3c21f17a269"
+});
+tpwidget("show");
---
+++ weather.js
@@ -0,0 +1,20 @@
+﻿(function(T, h, i, n, k, P, a, g, e) {
+  g = function() {
+    P = h.createElement(i);
+    a = h.getElementsByTagName(i)[0];
+    P.src = k;
+    P.charset = "utf-8";
+    P.async = 1;
+    a.parentNode.insertBefore(P, a)
+  };
+  T["ThinkPageWeatherWidgetObject"] = n;
+  T[n] || (T[n] = function() {
+    (T[n].q = T[n].q || []).push(arguments)
+  });
+  T[n].l = +new Date();
+  if (T.attachEvent) {
+    T.attachEvent("onload", g)
+  } else {
+    T.addEventListener("load", g, false)
+  }
+}(window, document, "script", "tpwidget", "https://widget.seniverse.com/widget/chameleon.js"))
