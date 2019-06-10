51276bbc77ea407806a7fcf969b814f6754622ea
Feng <48149924+imlufeng@users.noreply.github.com>
2019-06-10 20:43:29 +0800

    Update gaokao-s.js

--- gaokao-s.js
+++ gaokao-s.js
@@ -1,3 +1,3 @@
-var secdtimeFormat = "2020高考倒计时{d}天{h}小时{i}分钟{s}秒";
-var initime = "2020-06-07 09:00";
+var secdtimeFormat = "2020新年倒计时{d}天{h}小时{i}分钟{s}秒";
+var initime = "2020-01-01 00:00";
 showsecdtime();
