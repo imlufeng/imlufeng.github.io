b8518475fd2a786d42d25adf6685b45ee7dd5fb6
Feng <48149924+imlufeng@users.noreply.github.com>
2019-07-06 17:26:37 +0800

    Update title.js

--- title.js
+++ title.js
@@ -2,14 +2,14 @@ var OriginTitile = document.title;
 var titleTime;
 document.addEventListener('visibilitychange',
 function() {
-    if (document.hidden) {
-        document.title = '发呆- ( ゜- ゜)つロ ' + OriginTitile;
-        clearTimeout(titleTime);
-    } else {
-        document.title = '( • ̀ω•́ )✧被发现了~ ' + OriginTitile;
-        titleTime = setTimeout(function() {
-            document.title = OriginTitile;
-        },
-        2000);
-    }
+  if (document.hidden) {
+    document.title = '发呆 (●'◡'●)ﾉ ❤' + OriginTitile;
+    clearTimeout(titleTime);
+  } else {
+  document.title = '被发现了 ✧٩(ˊωˋ*)و✧' + OriginTitile;
+  titleTime = setTimeout(function() {
+  document.title = OriginTitile;
+    },
+    2000);
+  }
 });
