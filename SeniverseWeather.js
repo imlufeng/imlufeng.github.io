d6e59ef9d8c4bcbc12aa9a2460ddefa9b26ec210
imlufeng <48149924+imlufeng@users.noreply.github.com>
2020-11-11 15:12:03 +0800

    Update SeniverseWeather.js

--- SeniverseWeather.js
+++ SeniverseWeather.js
@@ -1,20 +1,12 @@
-﻿(function(T, h, i, n, k, P, a, g, e) {
-  g = function() {
-    P = h.createElement(i);
-    a = h.getElementsByTagName(i)[0];
-    P.src = k;
-    P.charset = "utf-8";
-    P.async = 1;
-    a.parentNode.insertBefore(P, a)
-  };
-  T["ThinkPageWeatherWidgetObject"] = n;
-  T[n] || (T[n] = function() {
-    (T[n].q = T[n].q || []).push(arguments)
-  });
-  T[n].l = +new Date();
-  if (T.attachEvent) {
-    T.attachEvent("onload", g)
-  } else {
-    T.addEventListener("load", g, false)
-  }
-}(window, document, "script", "tpwidget", "https://cdn.sencdn.com/widget/static/js/widget-cc5d550.js"))
+(function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
+  window.SeniverseWeatherWidget('show', {
+    flavor: "bubble",
+    location: "WSKM4Q76FR2E",
+    geolocation: true,
+    language: "zh-Hans",
+    unit: "c",
+    theme: "auto",
+    token: "ba0d4975-d435-4459-b32a-2bb6157bb08d",
+    hover: "enabled",
+    container: "tp-weather-widget"
+  })
